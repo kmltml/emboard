cc5b6ca168ec0f3ecf71493a76963da25b89fe41
Kamil Tomala <kamil.tomala@gmail.com>
2019-11-06 17:07:33 +0100

    Add some documentation to pitch calculations

--- Src/modules/oscillator.c
+++ Src/modules/oscillator.c
@@ -6,9 +6,10 @@
 static uint16_t base_periods[12];
 
 void oscillator_init() {
+  // 2^(1/12) represented as a fraction, for equal temperament calculation
   uint64_t num = 579788;
   uint64_t den = 547247;
-  base_periods[0] = 5379;
+  base_periods[0] = 5379; // 8.18 Hz
   for(size_t i = 1; i < 12; i++) {
     base_periods[i] = (uint64_t) base_periods[i - 1] * den / num;
     printf("%d\n", (int32_t) base_periods[i]);
@@ -16,13 +17,13 @@ void oscillator_init() {
 }
 
 void oscillator_reset(voice_entry* voice) {
-  voice->osc.phase = 75;
+  voice->osc.phase = 0;
 }
 
 void oscillator_generate(voice_entry* voice) {
   uint16_t phase = voice->osc.phase;
   uint16_t period = base_periods[voice->note % 12];
-  period >>= voice->note / 12;
+  period >>= voice->note / 12; // Divide by 2 for each octave
   const uint16_t amplitude = 0x5000;
   for(uint16_t i = 0; i < VOICE_BUFFER_SIZE; i++) {
     if(phase >= period) {
